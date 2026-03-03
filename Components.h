6bc091481b0f76ae522b57bc7b5269226e5cf706
Christian <chriatiancoirin@hotmail.com>
2026-03-02 19:55:04 -0600

    Completed Componenets.h file. Added asserts to check default contructed classes work.

--- Components.h
+++ Components.h
@@ -14,22 +14,28 @@ class CTransform : public Component
 {
 public:
 	Vec2f pos = { 0.0, 0.0 };
-	Vec2f velocity = { 0.0, 0.0 };
+	Vec2f prevPos = { 0.0, 0.0 };
 	Vec2f scale = { 1.0, 1.0 };
 	Vec2f velocity = { 0.0, 0.0 };
 	float angle = 0;
 
-	CTransform(const Vec2f& p, const Vec2f& v, float a)
-		:pos(p), velocity(v), angle(a)
+	CTransform() = default;
+	CTransform(const Vec2f& p)
+		:pos(p)
+	{
+	};
+	CTransform(const Vec2f& p, const Vec2f& v, const Vec2f& sc, float a)
+		:pos(p), prevPos(p), velocity(v), angle(a), scale(sc)
 	{
 	};
 };
 
-class CShape
+class CShape : public Component
 {
 public:
 	sf::CircleShape circle;
 
+	CShape() = default;
 	CShape(float radius, int points, const sf::Color& fill, const sf::Color& outline, float thickness)
 		:circle(radius, points)
 	{
@@ -40,32 +46,35 @@ public:
 	}
 };
 
-class CCollision
+class CCollision : public Component
 {
 public:
 	float radius = 0;
+	CCollision() = default;
 	CCollision(float r)
 		:radius(r) {}
 };
 
-class CScore
+class CScore : public Component
 {
 public:
+	CScore() = default;
 	int score = 0;
 	CScore(int s)
 		:score(s) {}
 };
 
-class CLifespan
+class CLifespan : public Component
 {
 public:
-	int remaining = 0; // remaining lifespan for entity 
-	int total = 0;     // total initial amount of lifespan
-	CLifespan(int total)
-		:remaining(total), total(total) {}
+	int lifespan = 0; // remaining lifespan for entity 
+	int frameCreated = 0;     // total initial amount of lifespan
+	CLifespan() = default;
+	CLifespan(int duration, int frame)
+		:lifespan(duration), frameCreated(frame) {}
 };
 
-class CInput
+class CInput : public Component
 {
 public:
 	bool up = false;
@@ -73,6 +82,63 @@ public:
 	bool right = false;
 	bool down = false;
 	bool shoot = false;
+	bool canShoot = true;
+	bool canJump = true;
 
-	CInput () {}
+	CInput() = default;
 };
+
+class CBoundingBox : public Component
+{
+public:
+	Vec2f size;
+	Vec2f halfSize;
+	CBoundingBox() = default;
+	CBoundingBox(const Vec2f& s)
+		:size(s), halfSize(s.x / 2, s.y / 2) {}
+};
+
+class CAnimation : public Component
+{
+public:
+	Animation animation;
+	bool repeat = false;
+	CAnimation() = default;
+	CAnimation(const Animation& animation, bool r)
+		: animation(animation)
+		, repeat(r) 
+	{}
+};
+
+class CGravity : public Component
+{
+public:
+	float gravity = 0;
+	CGravity() = default;
+	CGravity(float g) : gravity(g) {};
+};
+
+class CState : public Component
+{
+public:
+	std::string state = "jumping";
+	CState() = default;
+	CState(const std::string& s) : state(s) {};
+};
+
+
+// needed to store class in a tuple since class needs to be default constructible to be stored in a tuple and used in the Entity class
+static_assert(std::is_default_constructible_v<CTransform>);
+static_assert(std::is_default_constructible_v<CLifespan>);
+static_assert(std::is_default_constructible_v<CInput>);
+static_assert(std::is_default_constructible_v<CBoundingBox>);
+static_assert(std::is_default_constructible_v<CAnimation>);
+static_assert(std::is_default_constructible_v<CGravity>);
+static_assert(std::is_default_constructible_v<CState>);
+
+static_assert(std::is_default_constructible_v<Vec2f>);
+static_assert(std::is_default_constructible_v<Animation>);
+
+static_assert(std::is_default_constructible_v<CCollision>);
+static_assert(std::is_default_constructible_v<CScore>);
+static_assert(std::is_default_constructible_v<CShape>);
