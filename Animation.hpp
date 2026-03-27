028e783f46a994805c24f7fd4892824226b09191
Christian <chriatiancoirin@hotmail.com>
2026-03-27 09:56:15 -0500

    Completed animation class.

--- Animation.hpp
+++ Animation.hpp
@@ -12,11 +12,19 @@ class Animation
 	size_t m_speed = 0; // how many updates before we switch to the next frame
 	std::string m_name = "none";
 	std::string m_textureName = "none";
-	sf::IntRect m_textureRect;
+	sf::Sprite m_sprite;
+	Vec2f m_size;
 public:
 	Animation() = default;
+
 	Animation(const std::string& name, const std::string& textureName)
-		: Animation(name, textureName, 1, 0) {}
+		: Animation(name, textureName, 1, 0) 
+	{
+		const sf::Texture t = Assets::getInstance().getTexture(textureName);
+		m_size = Vec2f(t.getSize().x, t.getSize().y);
+		m_sprite.setOrigin(m_size.x / 2.0f, m_size.y / 2.0f);
+		m_sprite.setTextureRect(sf::IntRect(std::floor(m_currentFrame) * m_size.x, 0, m_size.x, m_size.y));
+	}
 
 	Animation(const std::string& name, const std::string& textureName, size_t framecount, size_t speed)
 		: m_name(name)
@@ -26,18 +34,31 @@ public:
 		, m_speed(speed)
 	{
 		const sf::Texture& t = Assets::getInstance().getTexture(textureName);
-		//m_textureRect.size = {int(t.getSize().x / framecount), int(t.getSize().y)};
+		m_size = Vec2f((float)t.getSize().x / framecount, (float)t.getSize().y);
+		m_sprite.setOrigin(m_size.x / 2.0f, m_size.y / 2.0f);
+		m_sprite.setTextureRect(sf::IntRect(std::floor(m_currentFrame) * m_size.x, 0, m_size.x, m_size.y));
 	}
+
 	void update()
 	{
 		m_currentFrame++;
 		if (m_speed == 0) return; // if speed is 0, we never switch frames
-		m_currentFrame = (m_currentFrame + 1) % m_framecount;
+		int frame = (m_currentFrame / m_speed) % m_framecount; // safe code would check for 'divide by zero' error
+		sf::IntRect rect = sf::IntRect(frame * (m_size.x), 0, m_size.x, m_size.y);
+
+		m_sprite.setTextureRect(rect);
+
 	}
+
 	bool hasEnded() const
 	{
-		return m_currentFrame == m_framecount - 1;
+		if (m_speed == 0) return false;
+		if (m_currentFrame >= m_framecount * m_speed) {
+			return true;
+		}
+		return false;
 	}
 	const std::string& getName() const { return m_name; }
-	const sf::IntRect& getRect() const { return m_textureRect; }
+	const Vec2f& getSize() const { return m_size; }
+	sf::Sprite& getSprite() { return m_sprite; }
 };
